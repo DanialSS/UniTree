08a8210cdf182f77c994b66ef58b6fb19240ea0e
DanialSS <danialserekov@gmail.com>
2022-05-24 15:33:52 +0600

    commit 12
    Login, Sign up, FAQ redesign

--- unitree/landing/urls.py
+++ unitree/landing/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from landing.views import Index, Navbar
+from landing.views import Index, Navbar, Faq
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('navbar/', Navbar.as_view(), name='navbar'),
+    path('faq/', Faq.as_view(), name='faq')
 ]
--- unitree/landing/views.py
+++ unitree/landing/views.py
@@ -9,3 +9,7 @@ class Index(View):
 class Navbar(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'landing/navbar.html')
+
+class Faq(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'landing/faq.html')
